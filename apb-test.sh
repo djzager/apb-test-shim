f5c75bcf1e4d31af4d989aa17a70d08509dcce54
David Zager <david.j.zager@gmail.com>
2018-02-21 15:26:36 -0500

    Add idempotence test

--- apb-test.sh
+++ apb-test.sh
@@ -1,38 +1,53 @@
 #!/bin/bash
 
+# Idea and code snippets taken from:
+# https://gist.github.com/geerlingguy/73ef1e5ee45d8694570f334be385e181
+
 # Exit on any individual command failure.
 set -e
-# Show trace.
-set -x
 
 # Pretty colors.
 red='\033[0;31m'
 green='\033[0;32m'
+yellow='\033[0;33m'
 neutral='\033[0m'
 
 export ANSIBLE_ROLES_PATH=$ANSIBLE_ROLES_PATH:$PWD/roles
+apb_name=${apb_name:-"test-apb"}
+test_idempotence=${test_idempotence:-"true"}
 
-printf ${green}"Setting up docker for insecure registry"${neutral}"\n"
+function run_apb() {
+    local apb_name=$1
+    local action=$2
+
+	printf ${green}"Running ${action} playbook"${neutral}
+	docker run --rm --net=host -v $HOME/.kube:/opt/apb/.kube:z -u $UID $apb_name $action --extra-vars "namespace=$apb_name"
+
+	if [ "$test_idempotence" = true ]; then
+		# Run Ansible playbook again (idempotence test).
+		printf ${green}"Running ${action} playbook again: idempotence test"${neutral}
+		idempotence=$(mktemp)
+		docker run --rm --net=host -v $HOME/.kube:/opt/apb/.kube:z -u $UID $apb_name $action --extra-vars "namespace=$apb_name" | tee -a $idempotence
+		tail $idempotence \
+		| grep -q 'changed=0.*failed=0' \
+		&& (printf ${green}'Idempotence test: pass'${neutral}"\n") \
+		|| (printf ${red}'Idempotence test: fail'${neutral}"\n" && exit 1)
+	fi
+}
+
+printf ${yellow}"Setting up docker for insecure registry"${neutral}"\n"
 sudo apt-get update -qq
 sudo sed -i "s/\DOCKER_OPTS=\"/DOCKER_OPTS=\"--insecure-registry=172.30.0.0\/16 /g" /etc/default/docker
 sudo cat /etc/default/docker
 sudo service docker restart
 printf "\n"
 
-printf ${green}"Installing requirements"${neutral}"\n"
+printf ${yellow}"Installing requirements"${neutral}"\n"
 pip install --pre ansible apb yamllint
 printf "\n"
 
 printf ${green}"Linting apb.yml"${neutral}"\n"
 yamllint apb.yml
-printf "\n"
-
-printf ${green}"Verify committed APB spec matches Dockerfile"${neutral}"\n"
-apb build
-if ! git diff --exit-code
-	then printf ${red}"Committed APB spec differs from built apb.yml spec"${neutral}"\n"
-    exit 1
-fi
 printf "\n"
 
 printf ${green}"Linting playbooks"${neutral}"\n"
@@ -42,14 +57,23 @@ done
 printf "\n"
 
 printf ${green}"Testing APB"${neutral}"\n"
-export APB_NAME=hello-world-apb
 apb build
+if ! git diff --exit-code
+	then printf ${red}"Committed APB spec differs from built apb.yml spec"${neutral}"\n"
+    exit 1
+fi
+
+printf ${yellow}"Bringing up openshift cluster, log in, and create project"${neutral}"\n"
 sudo docker cp $(docker create docker.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
 oc cluster up --version=$OPENSHIFT_VERSION
 oc login -u system:admin
-oc new-project $APB_NAME
-docker run --rm --net=host -v $HOME/.kube:/opt/apb/.kube:z -u $UID $APB_NAME provision --extra-vars "namespace=$APB_NAME"
-oc get all -n $APB_NAME
-docker run --rm --net=host -v $HOME/.kube:/opt/apb/.kube:z -u $UID $APB_NAME deprovision --extra-vars "namespace=$APB_NAME"
-oc get all -n $APB_NAME
+oc new-project $apb_name
+
+printf ${green}"Provision APB"${neutral}"\n"
+run_apb $apb_name provision
+oc get all -n $apb_name
+
+printf ${green}"Deprovision APB"${neutral}"\n"
+run_apb $apb_name deprovision
+oc get all -n $apb_name
 printf "\n"
