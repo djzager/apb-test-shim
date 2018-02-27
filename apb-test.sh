bce005e76800dd7975c21f02fd7657d6cb2c0d93
David Zager <david.j.zager@gmail.com>
2018-02-27 13:29:24 -0500

    Add kubernetes testing support

--- apb-test.sh
+++ apb-test.sh
@@ -14,38 +14,164 @@ neutral='\033[0m'
 
 apb_name=${apb_name:-"test-apb"}
 
-function run_apb() {
-    local image=$1
-    local namespace=$2
-    local action=$3
+function test_openshift() {
+    printf ${green}"Testing APB in OpenShift"${neutral}"\n"
+    echo -en 'travis_fold:start:openshift\\r'
+    printf ${yellow}"Setting up docker for insecure registry"${neutral}"\n"
+    sudo apt-get update -qq
+    sudo sed -i "s/\DOCKER_OPTS=\"/DOCKER_OPTS=\"--insecure-registry=172.30.0.0\/16 /g" /etc/default/docker
+    sudo cat /etc/default/docker
+    sudo service docker restart
+    sudo iptables -F
+    printf "\n"
 
-	printf ${green}"Running ${action} playbook"${neutral}"\n"
-	docker run --rm --net=host -v $HOME/.kube:/opt/apb/.kube:z -u $UID $image $action --extra-vars "namespace=$namespace"
+    printf ${yellow}"Bringing up an openshift cluster and logging in"${neutral}"\n"
+    sudo docker cp $(docker create docker.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
+    oc cluster up --routing-suffix=172.17.0.1.nip.io --public-hostname=172.17.0.1 --version=$OPENSHIFT_VERSION
+    oc login -u system:admin
+    docker build -t $apb_name -f Dockerfile .
+    printf "\n"
+
+    # Get enough permissions for APB to run
+    printf ${yellow}"Creating project sandbox for APB"${neutral}"\n"
+    oc new-project $apb_name
+    oc create serviceaccount -n $apb_name $apb_name
+    oc create clusterrolebinding $apb_name --clusterrole=cluster-admin --serviceaccount=$apb_name:$apb_name
+    printf "\n"
+    echo -en 'travis_fold:end:openshift\\r'
+
+    printf ${green}"Provision APB"${neutral}"\n"
+    oc run "$apb_name-provision" \
+        --namespace=$apb_name \
+        --env="POD_NAME=$apb_name-provision" \
+        --env="POD_NAMESPACE=$apb_name" \
+        --image=$apb_name \
+        --image-pull-policy=Never \
+        --restart=Never \
+        --attach=true \
+        --overrides='{ "spec": { "serviceAccountName": "'$apb_name'" } }' \
+        -- provision -e namespace=$apb_name -e cluster=openshift
+    oc get all -n $apb_name
+    printf "\n"
+
+    printf ${green}"Deprovision APB"${neutral}"\n"
+    oc run "$apb_name-deprovision" \
+        --namespace=$apb_name \
+        --env="POD_NAME=$apb_name-provision" \
+        --env="POD_NAMESPACE=$apb_name" \
+        --image=$apb_name \
+        --image-pull-policy=Never \
+        --restart=Never \
+        --attach=true \
+        --overrides='{ "spec": { "serviceAccountName": "'$apb_name'" } }' \
+        -- deprovision -e namespace=$apb_name -e cluster=openshift
+    oc get all -n $apb_name
+    printf "\n"
+
+    if [ -f "$PWD/tests/test.yml" ]; then
+        # TODO: Run test playbook(s)
+        printf ${green}"Test playbook exists"${neutral}"\n"
+    else
+        printf ${yellow}"No test playbook"${neutral}"\n"
+    fi
 }
 
-printf ${yellow}"Setting up docker for insecure registry"${neutral}"\n"
-sudo apt-get update -qq
-sudo sed -i "s/\DOCKER_OPTS=\"/DOCKER_OPTS=\"--insecure-registry=172.30.0.0\/16 /g" /etc/default/docker
-sudo cat /etc/default/docker
-sudo service docker restart
-printf "\n"
+function test_kubernetes() {
+    printf ${green}"Testing APB in Kubernetes"${neutral}"\n"
+
+    # https://github.com/kubernetes/minikube#linux-continuous-integration-without-vm-support
+    echo -en 'travis_fold:start:minikube\\r'
+    printf ${yellow}"Bringing up minikube"${neutral}"\n"
+    sudo curl -Lo /usr/bin/minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+    sudo chmod +x /usr/bin/minikube
+    sudo curl -Lo /usr/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+    sudo chmod +x /usr/bin/kubectl
+
+    export MINIKUBE_WANTUPDATENOTIFICATION=false
+    export MINIKUBE_WANTREPORTERRORPROMPT=false
+    export MINIKUBE_HOME=$HOME
+    export CHANGE_MINIKUBE_NONE_USER=true
+    mkdir $HOME/.kube || true
+    touch $HOME/.kube/config
+    export KUBECONFIG=$HOME/.kube/config
+
+    if [ "$KUBERNETES_VERSION" == "latest" ]; then
+        sudo minikube start --vm-driver=none
+    else
+        sudo minikube start --vm-driver=none --kubernetes-version=$KUBERNETES_VERSION
+    fi
+    minikube update-context
+    docker build -t $apb_name -f Dockerfile .
+    printf "\n"
+
+    # Get enough permissions for APB to run
+    printf ${yellow}"Creating project sandbox for APB"${neutral}"\n"
+    kubectl create namespace $apb_name
+    kubectl create serviceaccount -n $apb_name $apb_name
+    kubectl create clusterrolebinding $apb_name --clusterrole=cluster-admin --serviceaccount=$apb_name:$apb_name
+    printf "\n"
+    echo -en 'travis_fold:end:minikube\\r'
+
+    printf ${yellow}"Provision APB"${neutral}"\n"
+    kubectl run "$apb_name-provision" \
+        --namespace=$apb_name \
+        --env="POD_NAME=$apb_name-provision" \
+        --env="POD_NAMESPACE=$apb_name" \
+        --image=$apb_name \
+        --image-pull-policy=Never \
+        --restart=Never \
+        --attach=true \
+        --overrides='{ "spec": { "serviceAccountName": "'$apb_name'" } }' \
+        -- provision -e namespace=$apb_name -e cluster=kubernetes
+    kubectl get all -n $apb_name
+    printf "\n"
+
+    printf ${yellow}"Deprovision APB"${neutral}"\n"
+    kubectl run "$apb_name-deprovision" \
+        --namespace=$apb_name \
+        --env="POD_NAME=$apb_name-provision" \
+        --env="POD_NAMESPACE=$apb_name" \
+        --image=$apb_name \
+        --image-pull-policy=Never \
+        --restart=Never \
+        --attach=true \
+        --overrides='{ "spec": { "serviceAccountName": "'$apb_name'" } }' \
+        -- deprovision -e namespace=$apb_name -e cluster=kubernetes
+    kubectl get all -n $apb_name
+    printf "\n"
+
+    if [ -f "$PWD/tests/test.yml" ]; then
+        # TODO: Run test playbook(s)
+        printf ${green}"Test playbook exists"${neutral}"\n"
+    else
+        printf ${yellow}"No test playbook"${neutral}"\n"
+    fi
+}
 
 printf ${yellow}"Installing requirements"${neutral}"\n"
+echo -en 'travis_fold:start:install_requirements\\r'
 pip install --pre ansible apb yamllint
+echo -en 'travis_fold:end:install_requirements\\r'
 printf "\n"
 
 printf ${green}"Linting apb.yml"${neutral}"\n"
+echo -en 'travis_fold:start:lint.1\\r'
 yamllint apb.yml
+echo -en 'travis_fold:end:lint.1\\r'
 printf "\n"
 
-printf ${green}"Building apb"${neutral}"\n"
+printf ${green}"Preparing apb"${neutral}"\n"
+echo -en 'travis_fold:start:prepare.1\\r'
 apb build --tag $apb_name
 if ! git diff --exit-code
 	then printf ${red}"Committed APB spec differs from built apb.yml spec"${neutral}"\n"
     exit 1
 fi
+echo -en 'travis_fold:end:prepare.1\\r'
+printf "\n"
 
 printf ${green}"Linting playbooks"${neutral}"\n"
+echo -en 'travis_fold:start:lint.2\\r'
 playbooks=$(find playbooks -type f -printf "%f\n" -name '*.yml' -o -name '*.yaml')
 if [ -z "$playbooks" ]; then
     printf ${red}"No playbooks"${neutral}"\n"
@@ -54,28 +180,13 @@ fi
 for playbook in $playbooks; do
     docker run --entrypoint ansible-playbook $apb_name /opt/apb/actions/$playbook --syntax-check
 done
+echo -en 'travis_fold:end:lint.2\\r'
 printf "\n"
 
-printf ${green}"Testing APB"${neutral}"\n"
+if [ -n "$OPENSHIFT_VERSION" ]; then
+    test_openshift
+fi
 
-printf ${yellow}"Bringing up openshift cluster, log in, and create project"${neutral}"\n"
-sudo docker cp $(docker create docker.io/openshift/origin:$OPENSHIFT_VERSION):/bin/oc /usr/local/bin/oc
-oc cluster up --version=$OPENSHIFT_VERSION
-oc login -u system:admin
-oc new-project $apb_name
-
-printf ${green}"Provision APB"${neutral}"\n"
-run_apb $apb_name $apb_name provision $test_idempotence
-oc get all -n $apb_name
-
-printf ${green}"Deprovision APB"${neutral}"\n"
-run_apb $apb_name $apb_name deprovision $test_idempotence
-oc get all -n $apb_name
-printf "\n"
-
-if [ -f "$PWD/tests/test.yml" ]; then
-    # TODO: Run test playbook(s)
-    printf ${green}"Test playbook exists"${neutral}"\n"
-else
-    printf ${yellow}"No test playbook"${neutral}"\n"
+if [ -n "$KUBERNETES_VERSION" ]; then
+    test_kubernetes
 fi
