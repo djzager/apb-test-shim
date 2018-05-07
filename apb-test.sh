ff6821124b1060d4a4abe24aab4c1a3d3c71a444
David Zager <dzager@redhat.com>
2018-05-07 13:24:38 -0400

    Revert "Install pip requirements more reliably (#14)" (#15)
    This reverts commit 56062b05724cfca7e8a9fddc4c3c97eef6dc1755.

--- apb-test.sh
+++ apb-test.sh
@@ -137,9 +137,7 @@ function setup_kubernetes() {
 function requirements() {
     printf ${yellow}"Installing requirements"${neutral}"\n"
     echo -en 'travis_fold:start:install_requirements\\r'
-    export PATH=$HOME/.local/bin:$PATH
-    sudo pip install --pre apb yamllint --user `whoami`
-
+    pip install --pre ansible apb yamllint
     # Install nsenter
     docker run --rm jpetazzo/nsenter cat /nsenter > /tmp/nsenter 2> /dev/null; sudo cp /tmp/nsenter /usr/local/bin/; sudo chmod +x /usr/local/bin/nsenter; which nsenter
     echo -en 'travis_fold:end:install_requirements\\r'
